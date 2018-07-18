dced9552e7e2890725707e1687b13e6e65a12e80
Jonathan Nguyen <johnnynn95@gmail.com>
2018-07-18 23:58:09 +0300

    Partially added Loader

--- app/src/main/java/com/example/jnguyen/themovieapp/MainActivity.java
+++ app/src/main/java/com/example/jnguyen/themovieapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.jnguyen.themovieapp;
 
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
+import android.content.Loader;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,9 +18,14 @@ import android.widget.TextView;
 
 import com.example.jnguyen.themovieapp.Utilities.NetworkUtils;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>{
 
     int MOVIE_SEARCH_LOADER = 69;
+    private static final String SEARCH_QUERY_URL_EXTRA = "query";
     TextView mSearchQueryUrl;
     EditText mSearchQuery;
 
@@ -59,6 +68,43 @@ public class MainActivity extends AppCompatActivity {
             mSearchQueryUrl.setText(searchQueryResult);
             Log.v("searchQuery",searchQuery);
             Log.v("searchQueryResult",searchQueryResult);
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString(SEARCH_QUERY_URL_EXTRA,searchQueryResult);
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int i, final Bundle bundle) {
+        return new AsyncTaskLoader<String>(this) {
+            @Override
+            public String loadInBackground() {
+                String searchQueryUrl = bundle.getString(SEARCH_QUERY_URL_EXTRA);
+
+                if(searchQueryUrl == null || TextUtils.isEmpty(searchQueryUrl))
+                {
+                    return null;
+                }
+
+                try {
+                    URL url = new URL(searchQueryUrl);
+                    String searchQueryResults = NetworkUtils.getResponseFromHttpUrl(url);
+                    return searchQueryResults;
+                } catch (IOException e ){
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String s) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
     }
 }
 
